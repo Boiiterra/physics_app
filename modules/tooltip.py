e7b0558328996009458e4ff8df8f8b63f60ee1bb
TerraBoii <kostyaborisenko05@gmail.com>
2022-07-07 16:01:57 +0300

    Tooltip positioning now has "universal" formula - Refactored code ???

--- modules/tooltip.py
+++ modules/tooltip.py
@@ -3,11 +3,10 @@ from tkinter import Toplevel, Label
 
 
 class ToolTip:
-    def __init__(self, widget, id, lang):
+    def __init__(self, widget):
         self.tipwindow = None
         self.widget = widget
         self.x = self.y = 0
-        self.lang = lang
         self.id = id
 
     def showtip(self, text):
@@ -18,32 +17,12 @@ class ToolTip:
         self.tipwindow = tw = Toplevel(self.widget)
 
         mouse_x, mouse_y = mouse_pos()
-        if mouse_x <= 1700 and self.lang == "eng" and self.id == 0:
-            x = mouse_x + 14
-        elif mouse_x <= 1600 and self.lang == "rus" and self.id == 0:
-            x = mouse_x + 14
-        elif mouse_x > 1700 and self.lang == "eng" and self.id == 0:
-            x = mouse_x - 148
-        elif mouse_x > 1600 and self.lang == "rus" and self.id == 0:
-            x = mouse_x - 214
-        elif mouse_x <= 1700 and 0 < self.id < 7:
-            x = mouse_x + 14
-        elif mouse_x > 1700 and self.lang == "rus" and self.id == 1:
-            x = mouse_x - 165
-        elif mouse_x > 1700 and self.lang == "rus" and self.id == 2:
-            x = mouse_x - 140
-        elif mouse_x > 1700 and self.lang == "rus" and self.id == 3:
-            x = mouse_x - 150
-        elif mouse_x > 1700 and self.lang == "rus" and 4 <= self.id <= 6:
-            x = mouse_x - 200
-        elif mouse_x > 1700 and self.lang == "eng" and (self.id == 1 or self.id == 4):
-            x = mouse_x - 145
-        elif mouse_x > 1700 and self.lang == "eng" and (self.id == 2 or self.id == 5):
-            x = mouse_x - 150
-        elif mouse_x > 1700 and self.lang == "eng" and (self.id == 3 or self.id == 6):
-            x = mouse_x - 110
 
-        y = mouse_y + 1
+        if (tw.winfo_screenwidth() - mouse_x) < (12 * len(self.text) // 1.5) + 15:
+            x = int(tw.winfo_screenwidth() - ((12 * len(self.text) // 1.5) + 15))
+        else:
+            x = mouse_x + 15
+        y = mouse_y + 3
 
         tw.wm_overrideredirect(1)
         tw.wm_geometry("+%d+%d" % (x, y))
@@ -65,14 +44,7 @@ class ToolTip:
             tw.destroy()
 
 
-def create_tool_tip(widget, text, id, lang):
-    toolTip = ToolTip(widget, id, lang)
-
-    def enter(_):
-        toolTip.showtip(text)
-
-    def leave(_):
-        toolTip.hidetip()
-
-    widget.bind("<Enter>", enter)
-    widget.bind("<Leave>", leave)
+def create_tool_tip(widget, text):
+    toolTip = ToolTip(widget)
+    widget.bind("<Enter>", lambda _: toolTip.showtip(text))
+    widget.bind("<Leave>", lambda _: toolTip.hidetip())
